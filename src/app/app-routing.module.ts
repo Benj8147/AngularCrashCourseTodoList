505387ea758af5f1b34dda8b2928d38462cf9c69
Benjamin Grand <b.grand.hansen@gmail.com>
2019-08-08 09:40:43 +0200

    CourseDone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Routes, RouterModule, Router} from "@angular/router";
+import { TodosComponent } from "./components/todos/todos.component";
+import { AboutComponent } from "./components/pages/about/about.component";
 
+
+const routes: Routes = [
+  {path: '', component: TodosComponent},
+  {path: 'about', component: AboutComponent}
+];
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
